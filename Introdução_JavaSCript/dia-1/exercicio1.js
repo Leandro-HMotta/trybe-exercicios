82e82f5a54ea2ec22f4b2c1faebf1876f0887d14
Leandro Motta <leandromotta02@hotmail.com>
2022-11-25 09:17:23 -0300

    Exercicios JS

---
+++ Introdução_JavaSCript/dia-1/exercicio1.js
@@ -0,0 +1,9 @@
+const numeroMaior = 5;
+const numeroMenor = 5;
+
+if(numeroMaior > numeroMenor){
+  console.log(numeroMaior + ' é maior')
+}else if(numeroMenor > numeroMaior){
+  console.log(numeroMenor + ' é maior')
+}else {console.log('Os números são iguais')
+}
---
+++ Introdução_JavaSCript/dia-1/exericioIfElse.js
@@ -0,0 +1,17 @@
+const porcentagem = -01;
+
+if(porcentagem >= 90 && porcentagem < 101 ){
+  console.log('A');
+}else if (porcentagem >= 80 && porcentagem < 90){
+  console.log('B');
+}else if (porcentagem >= 70 && porcentagem < 80){
+  console.log('C');
+}else if (porcentagem >= 60 && porcentagem < 70){
+  console.log('D');
+}else if (porcentagem >= 50 && porcentagem < 60){
+  console.log('C');
+}else if (porcentagem < 50 && porcentagem > 0){
+  console.log('F');
+}else{
+  console.log('ERROR');
+}
