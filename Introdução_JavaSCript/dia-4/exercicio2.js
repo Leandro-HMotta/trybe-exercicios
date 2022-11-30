af47277e69eef1aabba671ad7aa3c1bdeef59946
Leandro Motta <leandromotta02@hotmail.com>
2022-11-30 18:29:32 -0300

    exericios trybe js dia4

---
+++ Introdução_JavaSCript/dia-4/exercicio2.js
@@ -0,0 +1,23 @@
+let leitor = {
+  nome: "Julia",
+  sobrenome: "Pessoa",
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: "O Pior Dia de Todos",
+      autor: "Daniela Kopsch",
+      editora: "Tordesilhas",
+    },
+  ],
+};
+console.log(
+  leitor.nome + " " + leitor.sobrenome + " " + leitor.livrosFavoritos[0].titulo
+);
+
+leitor.livrosFavoritos.push = {
+  titulo: "Harry Potter e o Prisioneiro de Azkaban",
+  autor: "JK Rowling",
+  editora: "Rocco",
+};
+
+console.table(leitor.livrosFavoritos);
